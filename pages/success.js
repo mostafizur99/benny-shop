c4f734dc588a5e6b9ebd1a5bb7fee496fcca594b
mostafizur99 <coder.mr11@gmail.com>
2022-09-21 10:13:18 +0600

    feat: order-success added

--- pages/success.js
+++ pages/success.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react'
+import useSWR from 'swr';
+import shootFireworks from '../lib/shoot-fireworks';
+import { fetcher } from '../src/utils/axiosKits';
 
 const SuccessPage = () => {
+    const {
+        query: { session_id },
+    } = useRouter();
+
+    const { data, error } = useSWR(() => `/api/checkout_sessions/${session_id}`, fetcher);
+
+    useEffect(() => {
+        if (data) {
+            shootFireworks();
+        }
+    }, [data]);
+
+
     return (
-        <div>Success</div>
+        <>
+            <Head>
+                <title>Successful Order</title>
+            </Head>
+            <div className='text-center py-10'>
+                <h4>Order Successful!!</h4>
+                <h3>Thanks For Your Order!</h3>
+            </div>
+        </>
     )
 }
 
