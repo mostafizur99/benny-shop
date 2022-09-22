bb44444cabf135ce9e56e9e8f35b44700073d79c
mostafizur99 <coder.mr11@gmail.com>
2022-09-22 22:10:11 +0600

    feat: products data added

---
+++ src/data/products.js
@@ -0,0 +1,16 @@
+const productsData = [
+    {
+        id: 'price_1Ljb20Eo2qD5WG05l86vqfSI',
+        title: 'Classic Watch',
+        price: 285,
+        currency: 'USD',
+    },
+    {
+        id: 'price_1Ljb4GEo2qD5WG05hee3IeLb',
+        title: 'Light Watch',
+        price: 225,
+        currency: 'USD',
+    }
+];
+
+export default productsData;
