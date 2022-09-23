000928ac6f48de9cf62acaf20b9b9a3f49a97ce6
mostafizur99 <coder.mr11@gmail.com>
2022-09-23 08:19:23 +0600

    feat: layout added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Layout from '../src/components/layouts/Layout'
 
 export default function Home() {
   return (
@@ -10,7 +10,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h2 className="text-slate-500 font-medium">Hello Benny Shop</h2>
+      <Layout>
+        <h2 className="text-slate-500 font-medium">Hello Benny Shop</h2>
+      </Layout>
     </>
   )
 }
