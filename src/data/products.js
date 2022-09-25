98b18d29d2ba715a1b5cf76756f2e3c863275ce5
mostafizur99 <coder.mr11@gmail.com>
2022-09-25 22:02:27 +0600

    fear: product data updated

--- src/data/products.js
+++ src/data/products.js
@@ -4,12 +4,14 @@ const productsData = [
         title: 'Classic Watch',
         price: 285,
         currency: 'USD',
+        thumb: '/img/products/product1.jpg'
     },
     {
         id: 'price_1Ljb4GEo2qD5WG05hee3IeLb',
         title: 'Light Watch',
         price: 225,
         currency: 'USD',
+        thumb: '/img/products/product2.jpg'
     }
 ];
 
